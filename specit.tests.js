33b471fd3496fe3c6439b9bb2fdbdf8266cdd181
fschwiet <fschwiet@gmail.com>
2010-07-27 21:45:55 -0700

    attempted to update tests
    Powershell is... not yet my friend
    
    $patterns = (
    "^(?<spacing>\s*)\((?<prefix>(.(?!\.shouldNot))*)\)(?<rest>\.shouldNot.*)$",
    "^(?<spacing>\s*)\((?<prefix>(.(?!\.should))*)\)(?<rest>\.should.*)$",
    "^(?<spacing>\s*)(?<prefix>(.(?!\.shouldNot))*.)(?<rest>\.shouldNot.*)$",
    "^(?<spacing>\s*)(?<prefix>(.(?!\.should))*.)(?<rest>\.should.*)$")
    
    Get-Content .\specit.tests.js |
    foreach-object {
        $thisLine = $null;
    
        foreach ($pattern in $patterns) {
            if ($_ -match $pattern) {
                $thisLine = $matches["spacing"] + "verify(" + $matches["prefix"] + ")" + $matches["rest"];
                break;
            }
        }
    
        if ($thisLine -eq $null) {
            $thisLine = $_;
        }
        $thisLine
    }  | set-content test.txt -encoding utf8

--- specit.tests.js
+++ specit.tests.js
@@ -2,79 +2,79 @@ loadHtmlFile("SpecIt.tests.html");
 
 describe("SpecIt", function() {
   it("should match on inclusion", function() {
-    [1, 2].should(include, 1);
-    [1, 2].should(include, 1, 2);
-    ({one: 1, two: 2}).should(include, "one");
+    verify([1, 2]).should(include, 1);
+    verify([1, 2]).should(include, 1, 2);
+    verify({one: 1, two: 2}).should(include, "one");
 
-    [1, 2].shouldNot(include, [1, 2]);
-    [1, 2].shouldNot(include, [1, 2], 1, 2);
-    [1, 2].shouldNot(include, 3);
-    [1, 2].shouldNot(include, 3, 4);
-    ({one: 1}).shouldNot(include, "two");
+    verify([1, 2]).shouldNot(include, [1, 2]);
+    verify([1, 2]).shouldNot(include, [1, 2], 1, 2);
+    verify([1, 2]).shouldNot(include, 3);
+    verify([1, 2]).shouldNot(include, 3, 4);
+    verify({one: 1}).shouldNot(include, "two");
 
-    "string".should(include, "string");
-    "string".should(include, "ring");
-    "string".should(include, "tr");
+    verify("string").should(include, "string");
+    verify("string").should(include, "ring");
+    verify("string").should(include, "tr");
 
-    "string".shouldNot(include, "  string");
-    "string".shouldNot(include, "string ");
-    "string".shouldNot(include, "cat");
+    verify("string").shouldNot(include, "  string");
+    verify("string").shouldNot(include, "string ");
+    verify("string").shouldNot(include, "cat");
   });
 
   it("should match on equality", function() {
-    "string".should(eql, "string");
-    (1).should(eql, 1);
-    true.should(eql, true);
+    verify("string").should(eql, "string");
+    verify(1).should(eql, 1);
+    verify(true).should(eql, true);
 
-    "string".shouldNot(eql, "junk");
-    [].shouldNot(eql, []);
-    ["tree"].shouldNot(eql, ["tree"]);
-    ({}).shouldNot(eql, {});
-    true.shouldNot(eql, false);
+    verify("string").shouldNot(eql, "junk");
+    verify([]).shouldNot(eql, []);
+    verify(["tree"]).shouldNot(eql, ["tree"]);
+    verify({}).shouldNot(eql, {});
+    verify(true).shouldNot(eql, false);
   });
 
   it("should match on similarity", function() {
-    "string".should(beSimilarTo, "string");
-    (1).should(beSimilarTo, 1);
-    true.should(beSimilarTo, true);
-    [].should(beSimilarTo, []);
-    ["tree"].should(beSimilarTo, ["tree"]);
-    ({}).should(beSimilarTo, {});
-    ({a: 1}).should(beSimilarTo, {"a": 1});
+    verify("string").should(beSimilarTo, "string");
+    verify(1).should(beSimilarTo, 1);
+    verify(true).should(beSimilarTo, true);
+    verify([]).should(beSimilarTo, []);
+    verify(["tree"]).should(beSimilarTo, ["tree"]);
+    verify({}).should(beSimilarTo, {});
+    verify({a: 1}).should(beSimilarTo, {"a": 1});
 
-    "string".shouldNot(beSimilarTo, "junk");
-    true.shouldNot(beSimilarTo, false);
-    ({a: 1}).shouldNot(beSimilarTo, {b: 1});
+    verify("string").shouldNot(beSimilarTo, "junk");
+    verify(true).shouldNot(beSimilarTo, false);
+    verify({a: 1}).shouldNot(beSimilarTo, {b: 1});
   });
 
   it("should match on truthiness", function() {
-    "string".should(be);
-    true.should(be);
-    (1).should(be);
+    verify("string").should(be);
+    verify(true).should(be);
+    verify(1).should(be);
 
-    "".shouldNot(be);
-    false.shouldNot(be);
-    (0).shouldNot(be);
+    verify("").shouldNot(be);
+    verify(false).shouldNot(be);
+    verify(0).shouldNot(be);
   });
 
   it("should match by type comparison", function() {
-    "string".should(beA, String);
-    (function() {}).should(beA, Function);
-    true.should(beA, Boolean);
-    ({}).should(beAn, Object);
-    [].should(beAn, Array);
-    (1).should(beA, Number);
-    /regular-expression/.should(beA, RegExp);
+    verify("string").should(beA, String);
+    verify(function() {}).should(beA, Function);
+    verify(true).should(beA, Boolean);
+    verify({}).should(beAn, Object);
+    verify([]).should(beAn, Array);
+    verify(1).should(beA, Number);
+    verify(/regular-expression/).should(beA, RegExp);
 
-    "string".shouldNot(beAn, Object);
-    "string".shouldNot(beA, Number);
-    [].shouldNot(beAn, Object);
+    verify("string").shouldNot(beAn, Object);
+    verify("string").shouldNot(beA, Number);
+    verify([]).shouldNot(beAn, Object);
   });
 
   it("should match against regular expressions", function() {
-    "string".should(match, /string/);
-    "202-555-1212".should(match, /\d{3}.\d{3}.\d{4}/);
-    "string".shouldNot(match, /\w{10}/);
+    verify("string").should(match, /string/);
+    verify("202-555-1212").should(match, /\d{3}.\d{3}.\d{4}/);
+    verify("string").shouldNot(match, /\w{10}/);
   });
 
   it("should match on method presence", function() {
@@ -84,10 +84,10 @@ describe("SpecIt", function() {
       methodAttr: function() {}
     };
 
-    myObject.should(respondTo, "methodAttr");
-    myObject.shouldNot(respondTo, "attribute1");
-    myObject.shouldNot(respondTo, "booleanAttr");
-    myObject.shouldNot(respondTo, "junkMethod");
+    verify(myObject).should(respondTo, "methodAttr");
+    verify(myObject).shouldNot(respondTo, "attribute1");
+    verify(myObject).shouldNot(respondTo, "booleanAttr");
+    verify(myObject).shouldNot(respondTo, "junkMethod");
 
     var Person = function(options) {
       this.name = options.name || "";
@@ -99,74 +99,74 @@ describe("SpecIt", function() {
     };
 
     var john = new Person({name: "John Doe", age: 35});
-    john.should(respondTo, "sayHi");
-    john.shouldNot(respondTo, "name");
-    john.shouldNot(respondTo, "age");
-    john.shouldNot(respondTo, "sayGoodbye");
+    verify(john).should(respondTo, "sayHi");
+    verify(john).shouldNot(respondTo, "name");
+    verify(john).shouldNot(respondTo, "age");
+    verify(john).shouldNot(respondTo, "sayGoodbye");
   });
 
   it("should match on less than", function() {
-    (  2).should(beLessThan, 5);
-    ( -5).should(beLessThan, 0);
-    (  0).should(beLessThan, 0.1);
-    "awesome".should(beLessThan, "great");
-    (  5).shouldNot(beLessThan, 3);
-    (0.1).shouldNot(beLessThan, 0);
-    (0.1).shouldNot(beLessThan, 0.05);
-    (  5).shouldNot(beLessThan, 5);
+    verify(  2).should(beLessThan, 5);
+    verify( -5).should(beLessThan, 0);
+    verify(  0).should(beLessThan, 0.1);
+    verify("awesome").should(beLessThan, "great");
+    verify(  5).shouldNot(beLessThan, 3);
+    verify(0.1).shouldNot(beLessThan, 0);
+    verify(0.1).shouldNot(beLessThan, 0.05);
+    verify(  5).shouldNot(beLessThan, 5);
   });
 
   it("should match on less than or equal to", function() {
-    (  2).should(beLessThanOrEqualTo, 5);
-    ( -5).should(beLessThanOrEqualTo, 0);
-    (  0).should(beLessThanOrEqualTo, 0.1);
-    (  5).should(beLessThanOrEqualTo, 5);
-    "awesome".should(beLessThanOrEqualTo, "great");
-    "great".should(beLessThanOrEqualTo, "great");
-    (0.1).should(beLessThanOrEqualTo, 0.1);
+    verify(  2).should(beLessThanOrEqualTo, 5);
+    verify( -5).should(beLessThanOrEqualTo, 0);
+    verify(  0).should(beLessThanOrEqualTo, 0.1);
+    verify(  5).should(beLessThanOrEqualTo, 5);
+    verify("awesome").should(beLessThanOrEqualTo, "great");
+    verify("great").should(beLessThanOrEqualTo, "great");
+    verify(0.1).should(beLessThanOrEqualTo, 0.1);
 
-    (  5).shouldNot(beLessThanOrEqualTo, 3);
-    (0.1).shouldNot(beLessThanOrEqualTo, 0);
-    (0.1).shouldNot(beLessThanOrEqualTo, 0.05);
+    verify(  5).shouldNot(beLessThanOrEqualTo, 3);
+    verify(0.1).shouldNot(beLessThanOrEqualTo, 0);
+    verify(0.1).shouldNot(beLessThanOrEqualTo, 0.05);
   });
 
   it("should match on greater than", function() {
-    (  2).should(beGreaterThan, 1);
-    ( -5).should(beGreaterThan, -10);
-    (  0).should(beGreaterThan, -0.1);
-    "awesome".should(beGreaterThan, "absolute");
-    (  5).shouldNot(beGreaterThan, 30);
-    (0.1).shouldNot(beGreaterThan, 0.2);
-    (0.01).shouldNot(beGreaterThan, 0.05);
-    (  5).shouldNot(beGreaterThan, 5);
+    verify(  2).should(beGreaterThan, 1);
+    verify( -5).should(beGreaterThan, -10);
+    verify(  0).should(beGreaterThan, -0.1);
+    verify("awesome").should(beGreaterThan, "absolute");
+    verify(  5).shouldNot(beGreaterThan, 30);
+    verify(0.1).shouldNot(beGreaterThan, 0.2);
+    verify(0.01).shouldNot(beGreaterThan, 0.05);
+    verify(  5).shouldNot(beGreaterThan, 5);
   });
 
   it("should match on greater than or equal to", function() {
-    (  2).should(beGreaterThanOrEqualTo, 1);
-    ( -5).should(beGreaterThanOrEqualTo, -10);
-    (  0).should(beGreaterThanOrEqualTo, -0.1);
-    (  5).should(beGreaterThanOrEqualTo, 5);
-    "awesome".should(beGreaterThanOrEqualTo, "awesome");
-    (  5).shouldNot(beGreaterThanOrEqualTo, 30);
-    (0.1).shouldNot(beGreaterThanOrEqualTo, 0.2);
-    (0.01).shouldNot(beGreaterThanOrEqualTo, 0.05);
+    verify(  2).should(beGreaterThanOrEqualTo, 1);
+    verify( -5).should(beGreaterThanOrEqualTo, -10);
+    verify(  0).should(beGreaterThanOrEqualTo, -0.1);
+    verify(  5).should(beGreaterThanOrEqualTo, 5);
+    verify("awesome").should(beGreaterThanOrEqualTo, "awesome");
+    verify(  5).shouldNot(beGreaterThanOrEqualTo, 30);
+    verify(0.1).shouldNot(beGreaterThanOrEqualTo, 0.2);
+    verify(0.01).shouldNot(beGreaterThanOrEqualTo, 0.05);
   });
 
   it("should match on emptiness", function() {
-    [].should(beEmpty);
-    ({}).should(beEmpty);
-    (0).should(beEmpty);
-    (5).should(beEmpty);
-    "".should(beEmpty);
-    [1, 2].shouldNot(beEmpty);
-    ({one: 1}).shouldNot(beEmpty);
-    "one".shouldNot(beEmpty);
+    verify([]).should(beEmpty);
+    verify({}).should(beEmpty);
+    verify(0).should(beEmpty);
+    verify(5).should(beEmpty);
+    verify("").should(beEmpty);
+    verify([1, 2]).shouldNot(beEmpty);
+    verify({one: 1}).shouldNot(beEmpty);
+    verify("one").shouldNot(beEmpty);
   });
 
   it("should match on elements on a page", function() {
    $(".workspace").append("<div class='great'>");
-   $(".workspace .great").should(beOnThePage);
-   $(".workspace .non-existant").shouldNot(beOnThePage);
+   verify($(".workspace .great")).should(beOnThePage);
+   verify($(".workspace .non-existant")).shouldNot(beOnThePage);
    $(".workspace").empty();
   });
 });
@@ -250,37 +250,37 @@ describe("SpecIt handling before and after", function() {
 
   it("should run before callbacks correctly", function() {
     $("#crazy").html("awesome div");
-    $("#crazy:contains(awesome div)").should(beOnThePage);
+    verify($("#crazy:contains(awesome div)")).should(beOnThePage);
   });
 
   it("should run after callbacks correctly", function() {
-    $("#crazy").length.should(eql, 1);
-    $("#crazy:contains(awesome div)").shouldNot(beOnThePage);
+    verify($("#crazy").length).should(eql, 1);
+    verify($("#crazy:contains(awesome div)")).shouldNot(beOnThePage);
   });
 });
 
 describe("SpecIt should know relative positions", function() {
   it("should know if an element is to the left of another", function() {
-    $(".left-right-correct .left").should(beToTheLeftOf, ".left-right-correct .right");
-    $(".left-right-correct .text-1").should(beToTheLeftOf, ".left-right-correct .text-2");
+    verify($(".left-right-correct .left")).should(beToTheLeftOf, ".left-right-correct .right");
+    verify($(".left-right-correct .text-1")).should(beToTheLeftOf, ".left-right-correct .text-2");
 
-    $(".left-right-correct .right").shouldNot(beToTheLeftOf, ".left-right-correct .left");
-    $(".left-right-broken .left").shouldNot(beToTheLeftOf, ".left-right-broken .right");
+    verify($(".left-right-correct .right")).shouldNot(beToTheLeftOf, ".left-right-correct .left");
+    verify($(".left-right-broken .left")).shouldNot(beToTheLeftOf, ".left-right-broken .right");
   });
 
   it("should know if an element is to the right of", function() {
-    $(".left-right-correct .right").should(beToTheRightOf, ".left-right-correct .left");
-    $(".left-right-correct .text-2").shouldNot(beToTheRightOf, ".left-right-correct .text-1");
+    verify($(".left-right-correct .right")).should(beToTheRightOf, ".left-right-correct .left");
+    verify($(".left-right-correct .text-2")).shouldNot(beToTheRightOf, ".left-right-correct .text-1");
 
-    $(".left-right-correct .left").shouldNot(beToTheRightOf, ".left-right-correct .right");
-    $(".left-right-broken .right").shouldNot(beToTheRightOf, ".left-right-broken .left");
+    verify($(".left-right-correct .left")).shouldNot(beToTheRightOf, ".left-right-correct .right");
+    verify($(".left-right-broken .right")).shouldNot(beToTheRightOf, ".left-right-broken .left");
   });
 
   it("should know if an element is to the above", function() {
-    $(".left-right-broken .left").should(beAbove, ".left-right-broken .right");
-    $(".left-right-correct .text-2").shouldNot(beAbove, ".left-right-correct .text-1");
+    verify($(".left-right-broken .left")).should(beAbove, ".left-right-broken .right");
+    verify($(".left-right-correct .text-2")).shouldNot(beAbove, ".left-right-correct .text-1");
 
-    $(".left-right-correct .left").shouldNot(beAbove, ".left-right-correct .right");
-    $(".left-right-correct .right").shouldNot(beAbove, ".left-right-correct .left");
+    verify($(".left-right-correct .left")).shouldNot(beAbove, ".left-right-correct .right");
+    verify($(".left-right-correct .right")).shouldNot(beAbove, ".left-right-correct .left");
   });
 });
